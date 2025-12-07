618594d80c39bdd9d1360a854869ce904fa01145
Sari Hennaert <r0262660@student.vives.be>
2025-12-07 23:57:37 +0100

    eerste stuk klassen

--- Examenopdrachtdecember2025.py
+++ Examenopdrachtdecember2025.py
@@ -2,4 +2,19 @@
 """
 Mijn examenopdracht voor Python December 2025
 """
-
+class Boeken:
+    def __init__(self , id, Titel, Auteur, UitgaveJaar):
+        self.id = id
+        self.Titel = Titel
+        self.Auteur = Auteur
+        self.UitgaveJaar = UitgaveJaar
+        
+class Locatie:
+    def __init__(self, id, boekID, NaamDepot, Plank):
+        self.id = id
+        self.boekID = boekID
+        self.NaamDepot = NaamDepot
+        self.Plank = Plank
+        
+import sqlite3 
+        
